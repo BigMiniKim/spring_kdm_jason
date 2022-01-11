aa15f1a2ec171919a27d4b59646f704cbfe43894
bigminikim <bigminikim@gmail.com>
2022-01-11 16:30:36 +0900

    first commit  MIME TYPE: MediaType.TEXT_PLAIN_VALUE

---
+++ src/main/java/edu/kosmo/ex/SampleController.java
@@ -0,0 +1,37 @@
+package edu.kosmo.ex;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.log4j.Log4j;
+
+
+
+
+//기존과는 다른, json으로 리턴시키기 위한 문법 적용 @RestController
+@RestController
+@Log4j
+@RequestMapping("/sample")
+public class SampleController {
+	
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText() {
+		
+		log.info("MIME TYPE: " + MediaType.TEXT_PLAIN_VALUE);
+		
+		return "안녕! 또 만났군~";
+	}
+	
+	
+}
---
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,36 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<html>
+<head>
+	<title>Home</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type = "text/javascript">
+$(document).ready( function() {
+	$.ajax({ 
+		type: "get", 
+		url : "${pageContext.request.contextPath}/sample/getText", 
+		success : function(data) { 
+			// Sucess시, 처리 
+			console.log(data); 
+		$("#hello").text(data)
+
+		}, 
+		error : function(xhr, textStatus, errorThrown){ 
+			// 	Error시, 처리 
+				alert(xhr); 
+				alert(textStatus); 	
+				alert(errorThrown);
+			} 
+		});	
+	
+	
+} );
+
+
+</script>
+</head>
+<body>
+<div id = "hello">
+
+</div>
+</body>
+</html>
